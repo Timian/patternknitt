27f08d511fbdc50e601796b07a7445c51c3ee34e
timian <jnuolsen@gmail.com>
2014-12-08 13:04:41 +0100

    comment the code in both files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#main.py is the main file, that is used for running the application.
+#imports diffrent function from kviy library.
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -5,18 +7,19 @@ from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 
-
+#these two just activate the function to the screen,so we can see
 class Strikkeapp(Screen):
     pass
 
 class MainScreenManager(ScreenManager):
     pass
 
+#loads the strikke.kv file(the design for the application) and build the strikkeapp
 class Strikkemain(App):
     sa=Strikkeapp()
     def build(self):
 	return Builder.load_file('strikke.kv')
-
+#runs the application
 Strikkemain().run()
 
 
